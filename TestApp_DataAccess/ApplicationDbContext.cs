61d71013c6ab2da2c29af0a516adfa67a6a33589
Pardhu <utlapallipardhu@gmail.com>
2023-10-09 16:45:24 +0530

    added dbcontext

---
+++ TestApp_DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TestApp_Models.Models;
+
+namespace TestApp_DataAccess
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS03;Database=TestDB;TrustServerCertificate=True;Trusted_Connection=True");
+        }
+    }
+}
